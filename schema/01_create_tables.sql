1ccf4149504e9078ecbb1b3b7082837209f92e94
StandardLee <92062551+StandardLee@users.noreply.github.com>
2026-06-22 16:13:53 -0400

    Create 01_create_tables.sql
    Create SQL indicating tables

---
+++ schema/01_create_tables.sql
@@ -0,0 +1,57 @@
+/* 
+NOTE: user-defined types like u_char003 are defined as not NULL by default, but Null/not NULL is still specified on each coulmn for clarity
+*/
+
+CREATE TABLE [dbo].[masStore](
+	[storecode] [dbo].[u_char003] NOT NULL,
+	[storetype] [dbo].[u_char001] NOT NULL,
+	[storename] [dbo].[u_varchar050] NOT NULL,
+  CONSTRAINT [PK_masStore] PRIMARY KEY CLUSTERED ([storecode] ASC)
+);
+GO
+
+CREATE TABLE [dbo].[slsOrder](
+  [storecode] [dbo].[u_char003] NOT NULL,
+	[sono] [dbo].[u_char008] NOT NULL,
+	[type] [dbo].[u_char001] NOT NULL,
+	[sodate] [dbo].[u_char008] NOT NULL,
+	[soamount] [dbo].[u_decimal] NOT NULL,
+	[taxamount] [dbo].[u_decimal] NOT NULL,
+	[totalamount] [dbo].[u_decimal] NOT NULL,
+  CONSTRAINT [PK_slsOrder] PRIMARY KEY CLUSTERED ([storecode] ASC, [sono] ASC)
+);
+GO
+
+CREATE TABLE [dbo].[slsOrderDetail](
+	[storecode] [dbo].[u_char003] NOT NULL,
+	[sono] [dbo].[u_char008] NOT NULL,
+	[sku] [dbo].[u_char008] NOT NULL,
+	[itmName] [dbo].[u_varchar200] NOT NULL,
+	[price] [dbo].[u_decimal] NOT NULL,
+	[qty] [dbo].[u_int] NOT NULL,
+	[salesamount] [dbo].[u_decimal] NOT NULL,
+	[taxamount] [dbo].[u_decimal] NOT NULL,
+	[UPC] [dbo].[u_varchar020] NOT NULL,
+  CONSTRAINT [PK_slsOrderDetail] PRIMARY KEY CLUSTERED ([storecode] ASC, [sono] ASC, [나] ASC)
+);
+GO
+
+CREATE TABLE [dbo].[itmList](
+	[sku] [dbo].[u_char008] NOT NULL,
+	[brand] [dbo].[u_varchar050] NOT NULL,
+	[itmName] [dbo].[u_varchar200] NOT NULL,
+	[retailprice] [dbo].[u_decimal] NOT NULL,
+	[status] [dbo].[u_status] NOT NULL,
+  CONSTRAINT [PK_itmList] PRIMARY KEY CLUSTERED ([sku] ASC)
+);
+GO
+
+CREATE TABLE [dbo].[inventory](
+ 	[storecode] [dbo].[u_char003] NOT NULL,
+	[sku] [dbo].[u_char008] NOT NULL,
+	[transdate] [dbo].[u_char008] NOT NULL,
+	[transtype] [dbo].[u_char001] NOT NULL,
+	[qty] [dbo].[u_int] NOT NULL,
+  CONSTRAINT [PK_inventory] PRIMARY KEY CLUSTERED ([storecode] ASC, [sku] ASC)
+);
+GO
