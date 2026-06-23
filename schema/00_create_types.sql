07b15a8f185bafd86fb39b7fd1af85fba1deaf25
StandardLee <92062551+StandardLee@users.noreply.github.com>
2026-06-22 20:41:26 -0400

    Update 00_create_types.sql
    add more types

--- schema/00_create_types.sql
+++ schema/00_create_types.sql
@@ -1,7 +1,31 @@
 -- User-defined types for the Retail_SQL_Portfolio schema
 
--- 3 characters store codes, not NULL
+-- characters, not NULL
+CREATE TYPE [dbo].[u_char001] FROM [char](1) NOT NULL
+GO
+  
 CREATE TYPE [dbo].[u_char003] FROM [char](3) NOT NULL
+GO
+  
+CREATE TYPE [dbo].[u_char008] FROM [char](8) NOT NULL
+GO
+
+-- varchar, not NULL
+CREATE TYPE [dbo].[u_varchar020] FROM [varchar](20) NOT NULL
+GO
+
+CREATE TYPE [dbo].[u_varchar050] FROM [varchar](50) NOT NULL
+GO
+
+CREATE TYPE [dbo].[u_varchar200] FROM [varchar](200) NOT NULL
+GO
+
+-- deciamal with two decimal places, not NULL
+CREATE TYPE [dbo].[u_decimal] FROM [decimal](10, 2) NOT NULL
+GO
+
+-- bigint, not Null
+CREATE TYPE [dbo].[u_int] FROM [bigint] NOT NULL
 GO
 
 -- status flag indicating active/inactive, not NULL
